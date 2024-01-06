b852f2b6fd954248a695ee0774659a96932cf9d4
biswa <maharanabiswa53@gmail.com>
2024-01-07 00:09:51 +0530

    create new file

---
+++ For_LOOP.java
@@ -0,0 +1,9 @@
+public class For_LOOP {
+    public static void main(String[] args) {
+        // int i=1;
+        for(int i=0;i<5;i++){
+            System.out.print(i + " ");
+        }
+        // System.out.println();
+    }
+}
